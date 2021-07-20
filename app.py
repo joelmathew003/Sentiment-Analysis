5a9c848a4769cb3531aeb69e4eada907eabdede7
Joel <joelsammathew@gmail.com>
2021-07-21 02:26:56 +0530

    included prob

--- app.py
+++ app.py
@@ -6,20 +6,16 @@ import librosa
 
 
 st.title("Sentiment Analysis from Audio")
-st.subheader("Aim: To give a prediction of the mood based on the audio")
+st.sidebar.subheader("Hi kunda")
+st.sidebar.info("Aim: To give a prediction of the mood of the speaker based on the audio")
 audio = st.file_uploader("Please upload your audio in .wav format", type=["wav"])
 if audio is not None:
-    #st.write(type(audio))
-    #st.write(dir(audio))
-    details = {"name":audio.name,"dir":audio.__dir__,"type":audio.type}
-    #st.write(details)
     signal, sample_rate = librosa.load(audio)
     mfcc = librosa.feature.mfcc(signal, sample_rate, n_mfcc=13, n_fft=2048, hop_length=512)
     mfcc = np.asarray([np.asarray(mfcc.T)])
-    #st.write(mfcc.shape)
     loaded_model = load_model("sentiment_analysis_model_v1.h5")
-
-    predicted_output = np.argmax(loaded_model.predict(mfcc))
+    prediction = loaded_model.predict(mfcc)
+    predicted_output = np.argmax(prediction)
     if(predicted_output==0):
         st.subheader("Prediction : Disgust")
     elif (predicted_output==1):
@@ -32,3 +28,14 @@ if audio is not None:
         st.subheader("Prediction : Fear")
     elif (predicted_output==5):
         st.subheader("Prediction : Angry")
+    if st.checkbox("Show Probability of each emotion"):
+        prob = {}
+        prob["Disgust"] = prediction.T[0]
+        prob["Happy"] = prediction.T[1]
+        prob["Sad"] = prediction.T[2]
+        prob["Neutral"] = prediction.T[3]
+        prob["Fear"] = prediction.T[4]
+        prob["Anger"] = prediction.T[5]
+        st.bar_chart(prob)
+        st.table(prob)
+    
